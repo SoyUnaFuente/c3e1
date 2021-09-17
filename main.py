38b19d216a5c0ce5eafd668a54b6cc129b3254d6
unknown <alexander.lafuente@correo.unimet.edu.ve>
2021-09-16 21:33:14 -0400

    Resuelto Con Bono

---
+++ main.py
@@ -0,0 +1,12 @@
+
+num = int(input("Escribe Un Numero: "))
+
+if num > 0:
+    print("El numero es positivo")
+else:
+    print("El numero es negativo")
+
+if (num % 2) == 0:
+    print(f"{num} es par")
+else:
+    print (f"{num} es inpar")
